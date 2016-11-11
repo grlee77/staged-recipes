05afddc44dde7ec6e4bfa39cdd090dddac1ca272
Gregory R. Lee <gregory.lee@cchmc.org>
2016-11-11 17:16:34 -0500

    add ismrmrd

---
+++ recipes/ismrmrd/bld.bat
@@ -0,0 +1,13 @@
+mkdir build
+cd build
+
+cmake .. ^
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" ^
+    -DHDF5_C_INCLUDE_DIR="${PREFIX}\include"
+    :: -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++"
+
+make
+make install
+
+cd tests
+test_ismrmrd
