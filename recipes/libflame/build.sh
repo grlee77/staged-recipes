d9374132a8f216f7366dbcd10731ee8d7f781304
Gregory R. Lee <grlee77@gmail.com>
2017-03-28 10:47:43 -0400

    flame recipe fails to find LAPACK symbols on OSX

--- recipes/libflame/build.sh
+++ recipes/libflame/build.sh
@@ -1,3 +1,9 @@
+# export CC=gcc
+# OSX build failed with gcc 4.8.  need newer version?
+# OSX build succeeds with clang
+
+# --enable-vector-intrinsics=sse doesn't work with clang
+
 if [ `uname` == Linux ]; then
 
     ./configure \
@@ -6,7 +12,9 @@ if [ `uname` == Linux ]; then
       --enable-vector-intrinsics=sse \
       --enable-static-build \
       --enable-dynamic-build \
-      --enable-cblas-interfaces
+      --enable-cblas-interfaces \
+      --with-cc=$CC
+      # --with-ranlib=${PREFIX}/lib/gcc-ranlib-6  # smallk build instructions use gcc6 and set this
 
       #--with-extra-cflags=-fPIC \
       # --enable-gpu
@@ -19,10 +27,12 @@ if [ `uname` == Darwin ]; then
     ./configure \
       --prefix=$PREFIX \
       --enable-multithreading=pthreads \
-      --enable-vector-intrinsics=sse \
       --enable-static-build \
       --enable-dynamic-build \
-      --enable-cblas-interfaces
+      --enable-cblas-interfaces \
+      --with-cc=$CC
+      # --enable-vector-intrinsics=sse \
+      # --with-ranlib=${PREFIX}/lib/gcc-ranlib-6   # smallk build instructions use gcc6 and set this
 fi
 
 make -j${CPU_COUNT}
