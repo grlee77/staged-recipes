591b8f58d1fea499b6b25b8c37cbf31836a77ab2
Gregory R. Lee <gregory.lee@cchmc.org>
2017-05-19 11:38:31 -0400

    point to the Boost in the Anaconda install

--- recipes/ismrmrd/build.sh
+++ recipes/ismrmrd/build.sh
@@ -7,17 +7,23 @@ cd build
 if [[ `uname` == 'Darwin' ]]; then
     # -stdlib=libc++ added to avoid clang error when linking to boost
     cmake .. \
+        -DCMAKE_BUILD_TYPE=RELEASE \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DHDF5_C_INCLUDE_DIR="${PREFIX}/include" \
-        -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++"
+        -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -stdlib=libc++" \
+        -DBoost_INCLUDE_DIR="${PREFIX}/include" \
+        -DBoost_LIBRARY_DIR_RELEASE="${PREFIX}/lib"
 else
     export LD_LIBRARY_PATH=""
     cmake .. \
+        -DCMAKE_BUILD_TYPE=RELEASE \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-        -DHDF5_C_INCLUDE_DIR="${PREFIX}/include"
+        -DHDF5_C_INCLUDE_DIR="${PREFIX}/include" \
+        -DBoost_INCLUDE_DIR="${PREFIX}/include" \
+        -DBoost_LIBRARY_DIR_RELEASE="${PREFIX}/lib"
 fi
 
-make
+make -j$CPU_COUNT
 make install
 
 ./tests/test_ismrmrd
